9f64f3975d8a9214c28a90edcb7114990ccd528a
Superashu <ashu@growthz.ai>
2024-12-27 17:52:53 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
+  
+  build: {
+    outDir: 'dist'
+  }
 });
